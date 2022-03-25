98d250c9a37525136812c63c2fcce3964de15bb0
mesor21 <menzdd21@gmail.com>
2022-03-25 19:04:48 +0300

    1%

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostram>
+
+using namespace std;
+
+class Array
+{
+private:
+	unsigned char array_function[];
+	int amount_of_element;
+public:
+	Array(int y){
+		count_arr = y;
+	}	
+ };
+
+class Dacimal
+{
+public:
+	Dacimal(){
+
+	};
+	~Dacimal();
+};
+class Octall{
+
+}
+
+int main(){
+	
+}
